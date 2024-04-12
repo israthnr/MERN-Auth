71fdc34268fb35546b2d294a3314db17010d0161
israthnr <israthnr.20@uom.lk>
2024-04-12 12:05:16 +0530

    add middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -31,3 +31,15 @@ app.get('/',(req, res) => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+//Path: api/routes/auth.route.js
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+});
