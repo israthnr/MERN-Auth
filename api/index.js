8f04f1bd291aed94aa5fddff732de92b2095d6d1
israthnr <israthnr.20@uom.lk>
2024-04-12 11:48:16 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -15,6 +16,8 @@ mongoose
 
 const app =  express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
@@ -27,3 +30,4 @@ app.get('/',(req, res) => {
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
